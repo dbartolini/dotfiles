5b0165f65c33d0fc99c1092002879a7c63d4cbfb
Daniele Bartolini <danyatk@gmail.com>
2016-04-15 11:07:27 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-ln -sf .bashrc    $HOME/.bashrc
-ln -sf .bash      $HOME/.bash
-ln -sf .vimrc     $HOME/.vimrc
-ln -sf .vim       $HOME/.vim
-ln -sf .vim/fonts $HOME/.fonts
+ln -sf $PWD/.bashrc    $HOME/.bashrc
+ln -sf $PWD/.bash      $HOME/.bash
+ln -sf $PWD/.vimrc     $HOME/.vimrc
+ln -sf $PWD/.vim       $HOME/.vim
+ln -sf $PWD/.vim/fonts $HOME/.fonts
 
 git config --global core.pager 'less -x1,5'
 
